c30bdb1086dd9418f3db3d5c40d13fcb3f2f505c
Sourav Sarker <souravsarker036@gmail.com>
2025-07-29 23:08:31 +0600

    variBLE Type of

---
+++ module-13/data_type.js
@@ -0,0 +1,10 @@
+// string type variable
+var name = "sourav";
+var country = 'bangladesh'
+console.log(name,country)
+
+// boolean
+
+var isRich = true;
+console.log(isRich)
+console.log(typeof name)
