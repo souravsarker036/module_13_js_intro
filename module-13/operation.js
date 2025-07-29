286e0e696d921e93a5f8e282fa7672cc917121de
Sourav Sarker <souravsarker036@gmail.com>
2025-07-30 00:21:15 +0600

    operators

---
+++ module-13/operation.js
@@ -0,0 +1,16 @@
+var priceA = 20;
+var priceB = 6;
+var total = priceA + priceB;
+var totalS = priceA - priceB;
+var totalD = priceA / priceB;
+var totalM = priceA % priceB;
+
+var price = 30;
+price += 10; //price = price + 10;
+// price -= 10; //price = price - 10;
+// price *= 10; //price = price * 10;
+console.log(total)
+console.log(totalS)
+console.log(totalD.toFixed(2))
+console.log(totalM)
+console.log(price)
